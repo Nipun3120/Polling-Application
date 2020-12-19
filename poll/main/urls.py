3ff78defbc442b71b3f80950f3ef344d88d15ac6
Nipun3120 <nipunverma39@gmail.com>
2020-12-19 22:41:26 +0530

    Slugify Category Page URL

--- poll/main/urls.py
+++ poll/main/urls.py
@@ -3,5 +3,5 @@ from main import views
 
 urlpatterns = [
     path('', views.Index.as_view()),
-    path('question/<int:pk>', views.Question.as_view(), name='question')
+    path('question/<slug>', views.Question.as_view(), name='question')
 ]
