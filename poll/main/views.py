5a0c310c52c4d74fd19e6b38db20b4c7c6b95118
Nipun3120 <nipunverma39@gmail.com>
2020-12-19 23:43:03 +0530

    used BaseDetailView

--- poll/main/views.py
+++ poll/main/views.py
@@ -1,11 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
+    FormView, 
 )
+from django.views.generic.detail import BaseDetailView
 
-
-from main import models
+from main import models, forms
 
 # Create your views here.
 
@@ -14,6 +16,18 @@ class Index(ListView):
     template_name = 'main/index.html'
 
 
-class Question(DetailView):
+class Question(BaseDetailView, FormView):
     model = models.Question
     template_name = 'main/question.html'
+    form_class = forms.AnswerForm
+
+    def form_valid(self, form):
+
+        # If the form is valid, save the associated model
+        form_object = form.save(commit = False)
+        form_object.user = self.request.user
+        form_object.save()
+
+        return HttpResponseRedirect('/')
+
+
